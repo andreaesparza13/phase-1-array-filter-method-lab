e2700e364b44ff45f19fc9da533c502ba43291a1
Andrea <acesparza05@gmail.com>
2022-06-30 12:54:28 -0500

    commit tree 49b0cf536c0f2c11bf7ed6f401fc7a2c7aea11ad

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(driverNames, string) {
+    const list = driverNames.filter(names => names.toUpperCase() === string.toUpperCase())
+    if (list) {
+        return list
+    } else {
+        return undefined
+    }
+}
+
+function fuzzyMatch(driverNames, string) {
+    const list = driverNames.filter(names => names[0] === string[0])
+    if (list) {
+        return list
+    } else {
+        return undefined
+    } 
+}
+
+function matchName(driverNames, string) {
+    const list = driverNames.filter(names => names.name === string)
+    if (list) {
+        return list
+    } else {
+        return undefined
+    } 
+}
